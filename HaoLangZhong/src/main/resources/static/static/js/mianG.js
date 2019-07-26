72fff02000382c2f2d53532169705d5ddcf853ed
guofeng-mi <guofeng_mi@sina.com>
2019-07-26 15:45:44 +0800

    logic726

--- HaoLangZhong/src/main/resources/static/static/js/mianG.js
+++ HaoLangZhong/src/main/resources/static/static/js/mianG.js
@@ -1,10 +1,10 @@
  let Global=(function(){
-  //var api="http://localhost:8980/js/f/sys/"
-  //var projectApi="http://localhost:8980/js/"      
+  var api="http://localhost:8980/js/f/sys/"
+  var projectApi="http://localhost:8980/js/"      
   //var api="http://192.168.1.28:8980/js/f/sys/"
   //var projectApi="http://192.168.1.28:8980/js/"      
-  var api="http://120.92.10.2:81/hlz/f/sys/"
-  var projectApi="http://120.92.10.2:81/hlz/"
+  //var api="http://120.92.10.2:81/hlz/f/sys/"
+  //var projectApi="http://120.92.10.2:81/hlz/"
   var picUrl="http://120.92.10.2/hlz/f/sys/fileInfo/showPic/"   
   //得到get 参数	   
    var getParameter=function(name){
